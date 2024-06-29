7b1deb4bc9765c0ff13027cb450731009bff98dc
hya <hw@gmail.com>
2024-06-30 01:42:37 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import duckdb
+import io
+import pandas as pd
+
+st.write('HEllo World!')
+
+tab1, tab2, tab3 = st.tabs(["Cat","Dog","Owl"])
+df = pd.read_csv(io.StringIO("""a,b,c
+    1,2,3
+    7,9,7"""))
+with tab1:
+    query = st.text_area(label="entrez votre input")
+    result = duckdb.sql(query).df()
+    st.write(f"Query entée : {query}")
+    st.dataframe(result)
+with tab2:
+    st.header("A dog")
+
+with tab3:
+    st.header('lkada')
