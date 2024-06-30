407c793c2f033d569023e6b6827a0f5c60ad6b63
hya <hw@gmail.com>
2024-06-30 23:14:24 +0200

    added sidebar

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import duckdb
 import io
 import pandas as pd
 
+
 beverages = pd.read_csv(io.StringIO("""
 beverage,price
 Orange juice,2.5
 Expresso,2
 Tea,3"""))
+
 
 food_items = pd.read_csv(io.StringIO("""
 food_item,food_price
@@ -15,11 +17,22 @@ Cookie juice,2.5
 Chocolatine,2
 Muffin,3"""))
 
+
 answer = """
 SELECT * FROM beverages
 CROSS JOIN food_items"""
 
 solution = duckdb.sql(answer).df()
+
+
+with st.sidebar:
+    option = st.selectbox(
+        "Sur quelle truc voulez-vous travailler?",
+        ("Jointures","Groupby","Window functions"),
+        index=None,
+        placeholder="Sélectionner le truc"
+    )
+    st.write('Vous avez sélectionné', option)
 
 
 st.header("enter your code:")
